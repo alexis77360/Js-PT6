61b963927771d30d4d7347ccb3bbb83a306ae73e
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-08-01 16:33:21 +0200

    canvas

--- draw-js/index.js
+++ draw-js/index.js
@@ -1,3 +1,47 @@
+const canvas = document.getElementById('art');
+const ctx = canvas.getContext("2d");
+
+function getMousePos(e){
+    //? obtenir la position de la souris dans le canvas
+    const rect = canvas.getBoundingClientRect();
+
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top,
+    };
+}
+
+//? Dessine en fonction de la position de la souris
+function mouseMove(e){
+    const mousePos = getMousePos(e);
+    ctx.lineTo(mousePos.x, mousePos.y);
+    ctx.stroke();
+    ctx.strokeStyle = 'green';
+    ctx.lineWidth = 5;
+}
+
+
+canvas.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    const mousePos = getMousePos(e);
+    ctx.beginPath();
+    ctx.moveTo(mousePos.x, mousePos.y);
+
+    //? dessiner une ligne
+    canvas.addEventListener('mousemove', mouseMove)
+
+    //? arrêter de dessiner
+    canvas.addEventListener('mouseup', () => {
+        canvas.removeEventListener('mousemove', mouseMove)
+    })
+});
+
+//? Effacer le canvas
+reset.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
+
+
 
 
 
